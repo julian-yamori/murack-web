6994bcaa08361ffaed9c7acaa2f6026db49a7840
julian-yamori <julian.lampyridae@gmail.com>
2025-08-16 09:36:33 +0000

    main.rs から ApiDoc とルーティング実装を api_define.rs に移動

---
+++ backend/src/api_define.rs
@@ -0,0 +1,74 @@
+//! バックエンド API 定義と、ルーティング実装
+
+use axum::{
+    Router,
+    routing::{get, put},
+};
+use murack_core_domain::SortType;
+use utoipa::OpenApi;
+
+use crate::{
+    AppState, artwork, group_list::handlers as group_handlers,
+    playlist::get_handlers as plist_gets, test_tag_group::handlers::*,
+};
+
+#[derive(OpenApi)]
+#[openapi(
+    paths(
+        artwork::get_mini_artwork,
+        group_handlers::get_genre_list,
+        group_handlers::get_artist_list,
+        group_handlers::get_album_list,
+        group_handlers::get_track_list,
+        plist_gets::get_playlist_list,
+        plist_gets::get_playlist_details,
+        get_tag_groups,
+        create_tag_group,
+        update_tag_group,
+        delete_tag_group
+    ),
+    components(schemas(SortType))
+)]
+pub struct ApiDoc;
+
+pub fn api_routing(mut router: Router<AppState>) -> Router<AppState> {
+    // artworks
+    router = router.route("/api/artworks/{id}/mini", get(artwork::get_mini_artwork));
+
+    // group_list
+    router = router
+        .route(
+            "/api/group_list/genre_list",
+            get(group_handlers::get_genre_list),
+        )
+        .route(
+            "/api/group_list/artist_list",
+            get(group_handlers::get_artist_list),
+        )
+        .route(
+            "/api/group_list/album_list",
+            get(group_handlers::get_album_list),
+        )
+        .route(
+            "/api/group_list/track_list",
+            get(group_handlers::get_track_list),
+        );
+
+    // playlists
+    router = router
+        .route("/api/playlists/list", get(plist_gets::get_playlist_list))
+        .route("/api/playlists/{id}", get(plist_gets::get_playlist_details));
+
+    // tag_groups (プロトタイプ用)
+    router = router
+        .route(
+            "/api/tag_groups",
+            get(get_tag_groups).post(create_tag_group),
+        )
+        .route(
+            "/api/tag_groups/{id}",
+            put(update_tag_group).delete(delete_tag_group),
+        );
+
+    router
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,4 @@
+mod api_define;
 mod artwork;
 mod database;
 mod error_handling;
@@ -8,37 +9,14 @@ mod track_list;
 
 use std::env;
 
-use axum::{
-    Router, middleware,
-    routing::{get, put},
-};
-use murack_core_domain::SortType;
+use axum::{Router, middleware};
+use sqlx::{Pool, Postgres};
 use tower_http::cors::{Any, CorsLayer};
 use utoipa::OpenApi;
 
-use crate::{
-    error_handling::error_handler_middleware, group_list::handlers as group_handlers,
-    playlist::get_handlers as plist_gets, test_tag_group::handlers::*,
-};
+use crate::{api_define::ApiDoc, error_handling::error_handler_middleware};
 
-#[derive(OpenApi)]
-#[openapi(
-    paths(
-        artwork::get_mini_artwork,
-        group_handlers::get_genre_list,
-        group_handlers::get_artist_list,
-        group_handlers::get_album_list,
-        group_handlers::get_track_list,
-        plist_gets::get_playlist_list,
-        plist_gets::get_playlist_details,
-        get_tag_groups,
-        create_tag_group,
-        update_tag_group,
-        delete_tag_group
-    ),
-    components(schemas(SortType))
-)]
-struct ApiDoc;
+type AppState = Pool<Postgres>;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -64,34 +42,7 @@ async fn main() -> anyhow::Result<()> {
         .allow_headers(Any);
 
     // Build our application with routes
-    let app = Router::new()
-        .route("/api/artworks/{id}/mini", get(artwork::get_mini_artwork))
-        .route(
-            "/api/group_list/genre_list",
-            get(group_handlers::get_genre_list),
-        )
-        .route(
-            "/api/group_list/artist_list",
-            get(group_handlers::get_artist_list),
-        )
-        .route(
-            "/api/group_list/album_list",
-            get(group_handlers::get_album_list),
-        )
-        .route(
-            "/api/group_list/track_list",
-            get(group_handlers::get_track_list),
-        )
-        .route("/api/playlists/list", get(plist_gets::get_playlist_list))
-        .route("/api/playlists/{id}", get(plist_gets::get_playlist_details))
-        .route(
-            "/api/tag_groups",
-            get(get_tag_groups).post(create_tag_group),
-        )
-        .route(
-            "/api/tag_groups/{id}",
-            put(update_tag_group).delete(delete_tag_group),
-        )
+    let app = api_define::api_routing(Router::new())
         .layer(middleware::from_fn(error_handler_middleware))
         .layer(cors)
         .with_state(pool);
